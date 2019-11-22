429d2ff78d3ca44671024388f065053fdd90f2b8
msweeneydev <mail@msweeneydev.com>
2019-11-22 16:39:29 +0000

    change var name

--- pages/api/user/exists/[email].js
+++ pages/api/user/exists/[email].js
@@ -3,12 +3,12 @@ import { client, q } from "../../_util/fauna";
 export default async (req, res) => {
   const { email } = req.query;
   try {
-    const dbs = await client.query(
+    const exists = await client.query(
       q.Exists(q.Match(q.Index("all_users_by_email"), email))
     );
-    console.log("Does user exists?", dbs);
+    console.log("Does user exist?", exists);
     // ok
-    res.status(200).json({ exists: dbs });
+    res.status(200).json({ existss });
   } catch (e) {
     // something went wrong
     res.status(500).json({ error: e.message });
