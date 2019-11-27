2361a93b2785f5e427a317ff52051179ac8e58df
msweeneydev <mail@msweeneydev.com>
2019-11-27 01:20:56 +0000

    comments

--- pages/api/auth/twitter/callback/index.js
+++ pages/api/auth/twitter/callback/index.js
@@ -3,12 +3,13 @@ const Twitter = require("twitter");
 
 export default (req, res) => {
   const { oauth_token, oauth_verifier } = req.query;
+  // get users access tokens
   oauth.getOAuthAccessToken(
     oauth_token,
     process.env.TWITTER_ACCESS_TOKEN_SECRET,
     oauth_verifier,
     async function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
-      // TODO: store access tokens in DB to use when required.
+      // verify credentials prior to saving in the database
       oauth.get(
         "https://api.twitter.com/1.1/account/verify_credentials.json",
         oauthAccessToken,
@@ -20,6 +21,7 @@ export default (req, res) => {
           } else {
             console.log("data is %j", data);
             data = JSON.parse(data);
+            // TODO: store access tokens in DB to use when required.
             res.writeHead(301, {
               Location: `/?accessToken=${oauthAccessToken}&accessTokenSecret=${oauthAccessTokenSecret}&name=${data.screen_name}`
             });
