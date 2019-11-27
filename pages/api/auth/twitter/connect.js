25360c6c39be0e3602b10e86f9b9607430207192
msweeneydev <mail@msweeneydev.com>
2019-11-27 00:35:00 +0000

    working well so far

--- pages/api/auth/twitter/connect.js
+++ pages/api/auth/twitter/connect.js
@@ -1,24 +1,23 @@
 const oauth = require("../../_util/oauth");
 
 export default (req, res) => {
-  console.log("CALLBACK", process.env.TWITTER_OAUTH_CALLBACK);
-  // TODO Find another library, because this one seems sketchy... forever getting 'token too old' at this step.
-  oauth.get(
-    "https://api.twitter.com/oauth/request_token",
-    process.env.TWITTER_ACCESS_TOKEN,
-    process.env.TWITTER_ACCESS_TOKEN_SECRET,
-    function(error, data, response) {
-      if (error) console.error("ERROR", error);
-      const obj = data.split("&").reduce(function(prev, curr, i, arr) {
-        const p = curr.split("=");
-        prev[decodeURIComponent(p[0])] = decodeURIComponent(p[1]);
-        return prev;
-      }, {});
-      console.log("OBJ", obj);
+  // TODO Stop request from getting cached, causes 'old request token' error.
+  oauth.getOAuthRequestToken(function(
+    error,
+    oauthToken,
+    oauthTokenSecret,
+    results
+  ) {
+    if (error) {
+      res.send(
+        "Error getting OAuth request token : " + util.inspect(error),
+        500
+      );
+    } else {
       res.writeHead(301, {
-        Location: `https://api.twitter.com/oauth/authorize?oauth_token=${obj.oauth_token}`
+        Location: `https://api.twitter.com/oauth/authorize?oauth_token=${oauthToken}`
       });
       res.end();
     }
-  );
+  });
 };
