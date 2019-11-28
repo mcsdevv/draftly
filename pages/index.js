1bebd93331a682dbe17391f53f2410a800453dc5
msweeneydev <mail@msweeneydev.com>
2019-11-28 01:20:08 +0000

    add todo

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import UserContext from "../context/UserContext";
 
 export default function Landing() {
   const { user } = useContext(UserContext);
+  // TODO Add hook to get new user data and reload
   console.log("USER", user);
   return (
     <>
