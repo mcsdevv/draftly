881f002451faf172966e5735f7e0b616eda1b739
msweeneydev <mail@msweeneydev.com>
2019-11-28 00:16:16 +0000

    add scope when creating team

--- pages/api/user/create/scope.js
+++ pages/api/user/create/scope.js
@@ -2,17 +2,24 @@ import { client, q } from "../../_util/fauna";
 
 export default async (req, res) => {
   const { email, name } = req.body;
+  console.log("CREATING SCOPE...");
+  // ! Currently overrides all scopes instead of merging
   try {
     const dbs = await client.query(
-      q.Update(q.Index("all_users_by_email"), email, {
-        data: {
-          scopes: {
-            name,
-            role: "owner",
-            type: "team"
+      q.Update(
+        q.Select(["ref"], q.Get(q.Match(q.Index("all_users_by_email"), email))),
+        {
+          data: {
+            scopes: [
+              {
+                name,
+                role: "owner",
+                type: "team"
+              }
+            ]
           }
         }
-      })
+      )
     );
     // ok
     res.status(200).json(dbs.data);
