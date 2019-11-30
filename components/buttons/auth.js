79837eb669d85f87ac68d63d0793190a823b70f2
msweeneydev <mail@msweeneydev.com>
2019-11-30 01:05:48 +0000

    fix logout

--- components/buttons/auth.js
+++ components/buttons/auth.js
@@ -1,18 +1,6 @@
 import Link from "next/link";
-import Cookies from "js-cookie";
-import { useRouter } from "next/router";
 
-export default ({ loggedIn, next }) => {
-  const router = useRouter();
-  const logout = async () => {
-    const res = await fetch("/api/auth/logout");
-    if (res.status === 200) {
-      Cookies.remove("id_token");
-      Cookies.remove("access_token");
-      localStorage.removeItem("user");
-      router.push("/");
-    }
-  };
+export default ({ loggedIn, logout, next }) => {
   return !loggedIn ? (
     <Link href={`/api/auth/login/${next || "dashboard"}`}>
       <button>Login</button>
