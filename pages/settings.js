c5c3013312f8a57eb1215c48826461a7c01c9d52
msweeneydev <mail@msweeneydev.com>
2019-12-01 01:27:39 +0000

    initial login and component creation

--- pages/settings.js
+++ pages/settings.js
@@ -1,13 +1,19 @@
 import { useContext } from "react";
 import UserContext from "../context/UserContext";
 
+import PersonalSettings from "../components/settings/personal";
+import TeamSettings from "../components/settings/team";
+
 export default function Settings() {
   const { scope, user } = useContext(UserContext);
+  console.log(scope, user);
+  const scopeType = user.scopes.filter(s => s.name === scope)[0].type;
+  console.log("typee", scopeType);
   return (
     <>
       <main>
         <h1>Settings Page</h1>
-        {scope}
+        {scopeType === "personal" ? <PersonalSettings /> : <TeamSettings />}
       </main>
       <style jsx>{``}</style>
     </>
