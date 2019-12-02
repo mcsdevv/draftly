f61d479ddee8a694c406e4407e1737ff7dbf6f4e
msweeneydev <mail@msweeneydev.com>
2019-12-02 23:56:06 +0000

    update dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -7,6 +7,12 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard</h1>
+      <h3>Hello {user.name}, the following items need your attention:</h3>
+      <ul>
+        <li>New Scheduled Tweets</li>
+        <li>New Drafts</li>
+        <li>Outstanding Reviews</li>
+      </ul>
       <style jsx>{``}</style>
     </>
   );
