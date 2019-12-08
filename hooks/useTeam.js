bc2bf621785fb50cb5410b87c61a355c96984373
msweeneydev <mail@msweeneydev.com>
2019-12-08 01:23:25 +0000

    conditional and dependent fetching

--- hooks/useTeam.js
+++ hooks/useTeam.js
@@ -4,22 +4,8 @@ import ScopeContext from "../context/scopeContext";
 
 export const useTeam = () => {
   const { scope } = useContext(ScopeContext);
-  const { data: team } = useSWR(
-    `/api/team/details/${(scope && scope.name) || "tets"}`
+  const { data: team } = useSWR(() =>
+    scope.type !== "personal" ? `/api/team/details/${scope.name}` : null
   );
   return { team };
 };
-
-// yarn add eslint-plugin-react-hooks --dev
-
-// const { scope } = useContext(ScopeContext);
-// const { data: team } = useSWR(
-//   `/api/team/details/${(scope && scope.name) || "tets"}`
-// );
-// return { ...team, scope };
-
-// const { scope } = useContext(ScopeContext);
-// if (scope && scope.type !== "personal") {
-//   const { data: team } = useSWR(`/api/team/details/${scope.name}`);
-//   return { ...team, scope };
-// }
