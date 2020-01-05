86d3397e7e1260dcdefa6d3846626fb0365a4f46
msweeneydev <mail@msweeneydev.com>
2020-01-05 02:40:09 +0000

    add preview api to get link meta

--- pages/tweets/drafts.js
+++ pages/tweets/drafts.js
@@ -7,12 +7,29 @@ import TweetsTabs from "../../components/tabs/tweets";
 import RequireLogin from "../../lib/requireLogin";
 
 function Drafts() {
+  const [draft, setDraft] = useState({ active: false, content: "" });
   const { user } = useProfile();
   const { scope } = useContext(ScopeContext);
+  const startDraft = () => {
+    setDraft({ ...draft, active: true });
+  };
+  const getPreview = async () => {
+    const res = await fetch("/api/preview", {
+      method: "POST",
+      body: JSON.stringify({
+        url: "https://twitter.com"
+      })
+    });
+    const resParse = await res.json();
+    console.log(resParse);
+  };
   return (
     <>
       <TweetsTabs />
-      <h1>Drafts</h1>
+      <h1 onClick={getPreview}>Drafts</h1>
+      <button onClick={startDraft}>Create Draft</button>
+      {draft.active && <textarea placeholder="Draft your tweet..." />}
+      <h2>Current Drafts:</h2>
       <style jsx>{``}</style>
     </>
   );
