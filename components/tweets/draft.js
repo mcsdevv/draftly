bf7d543fd43079c055e98a281921f46f1eb77d1b
msweeneydev <mail@msweeneydev.com>
2020-01-12 18:10:21 +0000

    minor

--- components/tweets/draft.js
+++ components/tweets/draft.js
@@ -18,10 +18,25 @@ export default function Draft({ revalidate, tweet }) {
       revalidate();
     }
   };
+  const handleReviewReady = async () => {
+    // setDeleting(true);
+    // const url = `/api/tweet/draft/delete/${scope.handle}`;
+    // const res = await fetch(url, {
+    //   method: "DELETE",
+    //   body: JSON.stringify({
+    //     ref: tweet.ref
+    //   })
+    // });
+    // if (res.status === 200) {
+    //   setDeleting(false);
+    //   revalidate();
+    // }
+  };
   return !deleting ? (
     <div className="draft">
       <h2>{tweet.text}</h2>
       <button onClick={handleDeleteDraft}>Delete Draft</button>
+      <button onClick={handleReviewReady}>Review Ready</button>
     </div>
   ) : (
     <h2>Deleting Draft...</h2>
