9f2405c2a584c0d5313ca5f31b106228580e3e9e
msweeneydev <mail@msweeneydev.com>
2020-01-20 11:24:22 +0000

    fix image height

--- components/tweets/cards/text.js
+++ components/tweets/cards/text.js
@@ -39,48 +39,6 @@ export default function Text({ meta, scope, text }) {
         .card-text :global(a:visited) {
           color: rgb(27, 149, 224);
         }
-        .card-link {
-          text-decoration: none;
-        }
-        .card-wrapper {
-          border: 1px solid rgb(204, 214, 221);
-          border-bottom-left-radius: 14px;
-          border-bottom-right-radius: 14px;
-          border-top-left-radius: 14px;
-          border-top-right-radius: 14px;
-          margin-top: 10px;
-        }
-        .card-image {
-          border-bottom: 1px solid rgb(204, 214, 221);
-        }
-        .card-image img {
-          border-top-left-radius: 14px;
-          border-top-right-radius: 14px;
-          max-width: 100%;
-        }
-        .card-content {
-          padding: 10px;
-        }
-        .card-title {
-          color: rgb(20, 23, 26);
-          font-size: 15px;
-          font-weight: 400;
-        }
-        .card-description {
-          color: rgb(101, 119, 134);
-          font-weight: 400;
-        }
-        .card-link {
-          display: flex;
-          color: rgb(101, 119, 134);
-          font-weight: 400;
-        }
-        .link {
-          fill: rgb(101, 119, 134);
-          margin-right: 5px;
-          margin-top: 2px;
-          width: 16.25px;
-        }
       `}</style>
     </>
   );
