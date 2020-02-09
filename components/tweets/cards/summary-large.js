bc457f139845b5acf73dac337b5ecd55b34645a4
msweeneydev <mail@msweeneydev.com>
2020-02-09 22:34:38 +0000

    summary large

--- components/tweets/cards/summary-large.js
+++ components/tweets/cards/summary-large.js
@@ -2,7 +2,7 @@ import Linkify from "react-linkify";
 
 import CardTop from "../card/top";
 import CardBottom from "../card/bottom";
-import { Box, Textarea } from "@chakra-ui/core";
+import { Box, Heading, Image, Text, Textarea } from "@chakra-ui/core";
 
 export default function SummaryLarge({
   editing,
@@ -37,14 +37,33 @@ export default function SummaryLarge({
           </Linkify>
         </p>
         <a className="card-link" href={`https://${meta.url}`}>
-          <div className="card-wrapper">
-            <div className="card-image">
-              <img src={meta.image} />
-            </div>
-            <div className="card-content">
-              <h3 className="card-title">{meta.title}</h3>
-              <p className="card-description">{meta.description}</p>
-              <div className="card-link">
+          <Box
+            border="1px solid rgb(204, 214, 221)"
+            borderRadius="14px"
+            marginTop="10px"
+          >
+            <Box borderBottom="1px solid rgb(204, 214, 221)" h="265px">
+              <Image
+                src={meta.image}
+                roundedTop="14px"
+                h="100%"
+                maxW="100%"
+                w="100%"
+              />
+            </Box>
+            <Box p="10px">
+              <Heading
+                as="h3"
+                color="rgb(20, 23, 26)"
+                fontSize="15px"
+                fontWeight="400"
+              >
+                {meta.title}
+              </Heading>
+              <Text color="rgb(101, 119, 134)" fontWeight="400">
+                {meta.description}
+              </Text>
+              <Box display="flex" color="rgb(101, 119, 134)" fontWeight="400">
                 <svg viewBox="0 0 24 24" className="link">
                   <g>
                     <path d="M11.96 14.945c-.067 0-.136-.01-.203-.027-1.13-.318-2.097-.986-2.795-1.932-.832-1.125-1.176-2.508-.968-3.893s.942-2.605 2.068-3.438l3.53-2.608c2.322-1.716 5.61-1.224 7.33 1.1.83 1.127 1.175 2.51.967 3.895s-.943 2.605-2.07 3.438l-1.48 1.094c-.333.246-.804.175-1.05-.158-.246-.334-.176-.804.158-1.05l1.48-1.095c.803-.592 1.327-1.463 1.476-2.45.148-.988-.098-1.975-.69-2.778-1.225-1.656-3.572-2.01-5.23-.784l-3.53 2.608c-.802.593-1.326 1.464-1.475 2.45-.15.99.097 1.975.69 2.778.498.675 1.187 1.15 1.992 1.377.4.114.633.528.52.928-.092.33-.394.547-.722.547z"></path>
@@ -52,9 +71,9 @@ export default function SummaryLarge({
                   </g>
                 </svg>
                 {new URL(`https://${meta.url}`, location).hostname}
-              </div>
-            </div>
-          </div>
+              </Box>
+            </Box>
+          </Box>
         </a>
         <CardBottom />
       </Box>
@@ -72,44 +91,6 @@ export default function SummaryLarge({
         }
         .card-link {
           text-decoration: none;
-        }
-        .card-wrapper {
-          border: 1px solid rgb(204, 214, 221);
-          border-bottom-left-radius: 14px;
-          border-bottom-right-radius: 14px;
-          border-top-left-radius: 14px;
-          border-top-right-radius: 14px;
-          margin-top: 10px;
-        }
-        .card-image {
-          border-bottom: 1px solid rgb(204, 214, 221);
-          height: 265px;
-        }
-        // ! 265px height and 507px width
-        .card-image img {
-          border-top-left-radius: 14px;
-          border-top-right-radius: 14px;
-          height: 100%;
-          max-width: 100%;
-          width: 100%;
-          // ! width:100% may not be the best idea
-        }
-        .card-content {
-          padding: 10px;
-        }
-        .card-title {
-          color: rgb(20, 23, 26);
-          font-size: 15px;
-          font-weight: 400;
-        }
-        .card-description {
-          color: rgb(101, 119, 134);
-          font-weight: 400;
-        }
-        .card-link {
-          display: flex;
-          color: rgb(101, 119, 134);
-          font-weight: 400;
         }
         .link {
           fill: rgb(101, 119, 134);
