c7e91220a14e24253d5ee27bd0df050618674340
msweeneydev <mail@msweeneydev.com>
2020-02-17 13:50:03 +0000

    increase text card width

--- components/tweets/cards/text.js
+++ components/tweets/cards/text.js
@@ -39,6 +39,9 @@ export default function Text({
         <CardBottom />
       </Box>
       <style jsx>{`
+        .card-text {
+          width: 500px;
+        }
         .card-text :global(a) {
           color: rgb(27, 149, 224);
           font-size: 15px;
