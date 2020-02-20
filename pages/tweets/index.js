137023d2a1083e5dcabd8690c7eca275be6b79c5
msweeneydev <mail@msweeneydev.com>
2020-02-20 11:56:04 +0000

    remove tabs

--- pages/tweets/index.js
+++ pages/tweets/index.js
@@ -1,13 +1,10 @@
 import { useRouter } from "next/router";
 
-import TweetsTabs from "../../components/tabs/tweets";
-
 import RequireLogin from "../../lib/requireLogin";
 
 function Tweets() {
   const router = useRouter();
   router.push("/tweets/drafts");
-  return <TweetsTabs />;
 }
 
 export default () => RequireLogin(Tweets);
