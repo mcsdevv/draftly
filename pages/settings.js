316f3b9ee79720eaf4af7d7a69ac5e3ed9ca8830
msweeneydev <mail@msweeneydev.com>
2020-02-23 20:50:53 +0000

    remove account

--- pages/settings.js
+++ pages/settings.js
@@ -2,7 +2,6 @@ import { useProfile } from "../hooks";
 
 import RequireLogin from "../lib/requireLogin";
 
-// import Account from "../components/settings/account";
 import Reviews from "../components/settings/reviews";
 import Plan from "../components/settings/plan";
 import Members from "../components/settings/members";
