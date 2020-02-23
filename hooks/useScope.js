5db1a3b1f18310b38eebcb080d071b15e6932299
msweeneydev <mail@msweeneydev.com>
2020-02-23 01:32:55 +0000

    simplify useScope

--- hooks/useScope.js
+++ hooks/useScope.js
@@ -1,16 +1,17 @@
 import { useContext } from "react";
 import ScopeContext from "../context/scopeContext";
+import { useRouter } from "next/router";
 
 import { useProfile } from "../hooks/";
 
 export const useScope = () => {
   const { scope, setScope } = useContext(ScopeContext);
   const { teams } = useProfile();
+  const router = useRouter();
   const updateScope = e => {
     const name = e.target.value;
     if (name === "new") {
-      localStorage.removeItem("scope");
-      window.location = "/api/auth/twitter/connect";
+      router.push("/api/auth/twitter/connect");
       return;
     }
     const scopeDetails = teams.find(t => t.handle === name);
