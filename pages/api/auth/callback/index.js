1f8cf3e58972ea473d4eba8b65fd35fbe59303fa
msweeneydev <mail@msweeneydev.com>
2020-02-25 19:26:04 +0000

    minor

--- pages/api/auth/callback/index.js
+++ pages/api/auth/callback/index.js
@@ -5,9 +5,9 @@ import cookieOptions from "../../_util/cookie/options";
 import { encrypt } from "../../_util/token/encryption";
 
 export default async (req, res) => {
-  //  confirm state match to mitigate CSRF
+  // * Confirm state match to mitigate CSRF
   if (req.query.state === req.cookies.state) {
-    // prepare options for token exchange
+    // * Prepare options for token exchange
     const authOptions = {
       method: "POST",
       url: `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
@@ -21,16 +21,15 @@ export default async (req, res) => {
       },
       json: true
     };
-    // send request for token exchange
+    // * Send request for token exchange
     const auth = await request(authOptions);
-    // check no error on token exchange
+    // * Check no error on token exchange
     if (!auth.error) {
       res.setHeader("Location", `${req.cookies.next}`);
-      console.log("CALLBACK NEXT", req.cookies.next);
       const id_token = jwt.decode(auth.id_token);
-      // encrypt access token
+      // * Encrypt access token
       const access_token = encrypt(auth.access_token);
-      //  confirm nonce match to mitigate token replay attack
+      // * Confirm nonce match to mitigate token replay attack
       if (req.cookies.nonce === id_token.nonce) {
         const existsOptions = {
           method: "GET",
@@ -41,7 +40,7 @@ export default async (req, res) => {
           json: true
         };
         const { exists } = await request(existsOptions);
-        // if user does not exist, create in db
+        // * If user does not exist, create in db
         if (!exists) {
           const createOptions = {
             method: "POST",
@@ -58,7 +57,7 @@ export default async (req, res) => {
           };
           await request(createOptions);
         }
-        // add id_token (browser) + access_token (httpOnly) as cookies
+        // * Add id_token (browser) + access_token (httpOnly) as cookies
         res.setHeader("Set-Cookie", [
           cookie.serialize(
             "id_token",
@@ -71,15 +70,15 @@ export default async (req, res) => {
             cookieOptions(true, false)
           )
         ]);
-        // send response
+        // * Send response using redirect code
         res.status(302).end();
       } else {
-        // advise token replay attack possible if nonce's do not match
+        // * Advise token replay attack possible if nonce's do not match
         res.send("Nonce mismatch, potential token replay attack underway.");
       }
     }
   } else {
-    // advise CSRF attack likely if states do not match
+    // * Advise CSRF attack likely if states do not match
     res.send("State mismatch, CSRF attack likely.");
   }
 };
--- pages/api/auth/login/index.js
+++ pages/api/auth/login/index.js
@@ -3,19 +3,18 @@ import uuidv4 from "uuid/v4";
 import cookieOptions from "../../_util/cookie/options";
 
 export default async (req, res) => {
-  // generate random opaque value for state and nonce
+  // * Generate random opaque value for state and nonce
   const state = uuidv4();
   const nonce = uuidv4();
-  // add state and nonce as httpOnly cookie for callback to check
+  // * Add state and nonce as httpOnly cookie for callback to check
   res.setHeader("Set-Cookie", [
     cookie.serialize("state", String(state), cookieOptions(true, false)),
     cookie.serialize("nonce", String(nonce), cookieOptions(true, false))
   ]);
-  // write redirect
+  // * Write redirect including openid, profile, and email scopes
   res.writeHead(302, {
     Location: `https://${process.env.AUTH0_DOMAIN}/authorize?response_type=code&audience=${process.env.AUTH0_AUDIENCE}&client_id=${process.env.AUTH0_CLIENT_ID}&redirect_uri=${process.env.AUTH0_REDIRECT_URI}/api/auth/callback&scope=openid%20profile%20email%20&state=${state}&nonce=${nonce}`
   });
-  // send response
   res.end();
 };
 
