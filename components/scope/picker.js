a52893a77c3bca5e91140d0dbd7f3935f8c5b169
msweeneydev <mail@msweeneydev.com>
2020-04-22 01:18:42 +0100

    picker

--- components/scope/picker.js
+++ components/scope/picker.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useScope, useProfile } from "../../hooks/";
 
+import Link from "../link";
 import Select from "../select";
 
 export default function ScopePicker() {
@@ -11,11 +12,13 @@ export default function ScopePicker() {
       setScope({ ...teams[0] });
     }
   }, [teams]);
-  return scope ? (
+  return scope && teams?.length > 0 ? (
     <Select
       onChange={updateScope}
       options={teams}
       value={(scope && scope.handle) || scope.name}
     />
-  ) : null;
+  ) : (
+    <Link href="/api/auth/twitter/connect">+ Add New Team</Link>
+  );
 }
