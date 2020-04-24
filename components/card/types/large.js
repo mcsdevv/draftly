027e0867b10f5894787db32e6fb94cb3fd475f2d
msweeneydev <mail@msweeneydev.com>
2020-04-24 14:17:30 +0100

    large card

--- components/card/types/large.js
+++ components/card/types/large.js
@@ -1,6 +1,5 @@
 import Linkify from "react-linkify";
 
-import { Box, Heading, Icon, Image, Text } from "@chakra-ui/core";
 import CardTop from "../sections/top";
 import CardBottom from "../sections/bottom";
 
@@ -21,12 +20,7 @@ export default function Large({
     <div className={styles.cardLarge}>
       <CardTop handle={scope.handle} name={scope.name} />
       <p className={styles.cardText}>
-        <Linkify
-          properties={{
-            target: "_blank",
-            style: { color: "red", fontWeight: "bold" },
-          }}
-        >
+        <Linkify>
           {!editing ? (
             text
           ) : (
@@ -36,43 +30,24 @@ export default function Large({
       </p>
       {editing && <Characters progress={(editTweet.length / 280) * 100} />}
       <a className={styles.cardLink} href={`https://${meta.url}`}>
-        <Box
-          border="1px solid rgb(204, 214, 221)"
-          borderRadius="14px"
-          marginTop="10px"
-        >
-          <Box borderBottom="1px solid rgb(204, 214, 221)" h="265px">
-            <Image
-              src={meta.image}
-              roundedTop="14px"
-              h="100%"
-              maxW="100%"
-              w="100%"
-            />
-          </Box>
-          <Box p="10px">
-            <Heading
-              as="h3"
-              color="rgb(20, 23, 26)"
-              fontSize="15px"
-              fontWeight="400"
-            >
-              {meta.title}
-            </Heading>
-            <Text color="rgb(101, 119, 134)" fontWeight="400">
-              {meta.description}
-            </Text>
-            <Box
-              alignItems="center"
-              display="flex"
-              color="rgb(101, 119, 134)"
-              fontWeight="400"
-            >
-              <Icon marginRight="2px" marginTop="2px" name="link" />
-              <Text>{new URL(`https://${meta.url}`, location).hostname}</Text>
-            </Box>
-          </Box>
-        </Box>
+        <div className={styles.cardContainer}>
+          <div className={styles.imageContainer}>
+            <img className={styles.image} src={meta.image} />
+          </div>
+          <div className={styles.contentContainer}>
+            <span className={styles.title}>{meta.title}</span>
+            <p className={styles.description}>{meta.description}</p>
+            <div className={styles.linkContainer}>
+              <svg className={styles.link} viewBox="0 0 24 24">
+                <g>
+                  <path d="M11.96 14.945c-.067 0-.136-.01-.203-.027-1.13-.318-2.097-.986-2.795-1.932-.832-1.125-1.176-2.508-.968-3.893s.942-2.605 2.068-3.438l3.53-2.608c2.322-1.716 5.61-1.224 7.33 1.1.83 1.127 1.175 2.51.967 3.895s-.943 2.605-2.07 3.438l-1.48 1.094c-.333.246-.804.175-1.05-.158-.246-.334-.176-.804.158-1.05l1.48-1.095c.803-.592 1.327-1.463 1.476-2.45.148-.988-.098-1.975-.69-2.778-1.225-1.656-3.572-2.01-5.23-.784l-3.53 2.608c-.802.593-1.326 1.464-1.475 2.45-.15.99.097 1.975.69 2.778.498.675 1.187 1.15 1.992 1.377.4.114.633.528.52.928-.092.33-.394.547-.722.547z"></path>
+                  <path d="M7.27 22.054c-1.61 0-3.197-.735-4.225-2.125-.832-1.127-1.176-2.51-.968-3.894s.943-2.605 2.07-3.438l1.478-1.094c.334-.245.805-.175 1.05.158s.177.804-.157 1.05l-1.48 1.095c-.803.593-1.326 1.464-1.475 2.45-.148.99.097 1.975.69 2.778 1.225 1.657 3.57 2.01 5.23.785l3.528-2.608c1.658-1.225 2.01-3.57.785-5.23-.498-.674-1.187-1.15-1.992-1.376-.4-.113-.633-.527-.52-.927.112-.4.528-.63.926-.522 1.13.318 2.096.986 2.794 1.932 1.717 2.324 1.224 5.612-1.1 7.33l-3.53 2.608c-.933.693-2.023 1.026-3.105 1.026z"></path>
+                </g>
+              </svg>
+              <span>{new URL(`https://${meta.url}`, location).hostname}</span>
+            </div>
+          </div>
+        </div>
       </a>
       <CardBottom />
     </div>
