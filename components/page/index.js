7ff318cc3446833fed007d7e8cc07a2dfb0229cc
msweeneydev <mail@msweeneydev.com>
2020-04-26 21:18:02 +0100

    optional divider

--- components/page/index.js
+++ components/page/index.js
@@ -3,12 +3,12 @@ import styles from "./page.module.css";
 import Divider from "../divider";
 import Header from "../header";
 
-export default function Page({ children, name }) {
+export default function Page({ children, divider = true, name }) {
   return (
     <div className={styles.page}>
       <Header />
       <h2 className={styles.heading}>{name}</h2>
-      <Divider />
+      {divider && <Divider />}
       {children}
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,11 @@
-import { Heading } from "@chakra-ui/core";
-import Button from "../components/button";
-import Link from "../components/link";
-import Input from "../components/input";
-
 import Page from "../components/page";
 
 export default function Landing() {
   return (
-    <Page>
-      <Heading as="h2">Draft...</Heading>
-      <Heading as="h2">Review...</Heading>
-      <Heading as="h2">Publish...</Heading>
-      <Button disabled>Drafts</Button>
-      <Button type="tertiary">Published</Button>
-      <Link href="test" type="secondary">
-        Nice link
-      </Link>
-      <Input placeholder="Email..." withButton buttonText="Hey y'all!" />
-      <Input placeholder="Email..." />
-      <Input placeholder="Email..." />
+    <Page divider={false}>
+      <h2>Draft...</h2>
+      <h2>Review...</h2>
+      <h2>Publish...</h2>
     </Page>
   );
 }
