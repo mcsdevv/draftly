a1101cab824b0744a30d055f19c65ed49d198fe2
msweeneydev <mail@msweeneydev.com>
2020-05-01 19:53:23 +0100

    fix scope error

--- components/scope/picker.js
+++ components/scope/picker.js
@@ -12,13 +12,9 @@ export default function ScopePicker() {
       setScope({ ...teams[0] });
     }
   }, [teams]);
-  return teams !== undefined ? (
+  return teams !== undefined && scope ? (
     teams.length > 0 ? (
-      <Select
-        onChange={updateScope}
-        options={teams}
-        value={(scope && scope.handle) || scope.name}
-      />
+      <Select onChange={updateScope} options={teams} value={scope.handle} />
     ) : (
       <Link href="/api/auth/twitter/connect">+ Add New Team</Link>
     )
