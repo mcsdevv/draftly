09d0fb7657046b30c51a3050e05c11f412e491e3
msweeneydev <mail@msweeneydev.com>
2020-05-02 18:41:43 +0100

    minor

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,42 @@
-import { useEffect, useState } from "react";
+import App from "next/app";
 import ScopeContext from "../context/scopeContext";
 import { SWRConfig } from "swr";
 
 import "../styles/global.css";
 
-const App = ({ Component, pageProps }) => {
-  const [scope, setScope] = useState(undefined);
-  useEffect(() => {
-    function getStoredScope() {
-      const scopeStored = localStorage.getItem("scope");
-      if (scopeStored !== undefined && scopeStored !== null) {
-        const scope = JSON.parse(scopeStored);
-        setStoredScope(scope);
-      }
-    }
-    getStoredScope();
-  }, []);
-  const setStoredScope = (newScope) => {
-    localStorage.setItem("scope", JSON.stringify(newScope));
-    setScope(newScope);
+export default class MyApp extends App {
+  state = {
+    scope: undefined,
   };
-  return (
-    <SWRConfig
-      value={{
-        refreshInterval: 0,
-        fetcher: (...args) => fetch(...args).then((res) => res.json()),
-      }}
-    >
-      <ScopeContext.Provider value={{ scope, setScope: setStoredScope }}>
-        <Component {...pageProps} />
-      </ScopeContext.Provider>
-    </SWRConfig>
-  );
-};
-
-export default App;
+  componentDidMount = () => {
+    const scopeStored = localStorage.getItem("scope");
+    if (scopeStored !== undefined && scopeStored !== null) {
+      const scope = JSON.parse(scopeStored);
+      this.setScope(scope);
+    }
+  };
+  setScope = (scope) => {
+    localStorage.setItem("scope", JSON.stringify(scope));
+    this.setState({ scope });
+  };
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <SWRConfig
+        value={{
+          refreshInterval: 0,
+          fetcher: (...args) => fetch(...args).then((res) => res.json()),
+        }}
+      >
+        <ScopeContext.Provider
+          value={{
+            scope: this.state.scope,
+            setScope: this.setScope,
+          }}
+        >
+          <Component {...pageProps} />
+        </ScopeContext.Provider>
+      </SWRConfig>
+    );
+  }
+}
