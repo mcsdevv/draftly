6c453cd57a54c304885f7d5aae39ffd153fff5c8
msweeneydev <mail@msweeneydev.com>
2020-05-02 18:29:43 +0100

    reset scope

--- components/scope/picker.js
+++ components/scope/picker.js
@@ -1,8 +1,6 @@
 import { useEffect } from "react";
 import { useScope, useProfile } from "../../hooks/";
 
-import Cookies from "js-cookie";
-
 import Link from "../link";
 import Select from "../select";
 
@@ -10,17 +8,17 @@ export default function ScopePicker() {
   const { scope, setScope, updateScope } = useScope();
   const { teams } = useProfile();
   useEffect(() => {
-    let newScope = null;
-    if (!scope && teams?.length > 0 && Cookies.get("id_token")) {
-      newScope = teams[0];
+    if (scope === null && teams) {
+      setScope({ ...teams[0] });
     }
-    setScope(newScope);
   }, [teams]);
-  return teams !== undefined && scope ? (
-    teams.length > 0 ? (
-      <Select onChange={updateScope} options={teams} value={scope.handle} />
-    ) : (
-      <Link href="/api/auth/twitter/connect">+ Add New Team</Link>
-    )
-  ) : null;
+  return scope && teams?.length > 0 ? (
+    <Select
+      onChange={updateScope}
+      options={teams}
+      value={(scope && scope.handle) || scope.name}
+    />
+  ) : (
+    <Link href="/api/auth/twitter/connect">+ Add New Team</Link>
+  );
 }
