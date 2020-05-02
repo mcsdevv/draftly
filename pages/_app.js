dc02cbf379fa06b5c1165e50326da038134258b3
msweeneydev <mail@msweeneydev.com>
2020-05-02 16:32:49 +0100

    functional _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,87 +1,38 @@
-import App from "next/app";
+import { useEffect, useState } from "react";
 import ScopeContext from "../context/scopeContext";
 import { SWRConfig } from "swr";
 
 import "../styles/global.css";
 
-export default class MyApp extends App {
-  state = {
-    scope: undefined,
-  };
-  componentDidMount = () => {
-    const scopeStored = localStorage.getItem("scope");
-    if (scopeStored !== undefined && scopeStored !== null) {
-      const scope = JSON.parse(scopeStored);
-      console.log("setting scope - CLASS");
-      this.setScope(scope);
+const App = ({ Component, pageProps }) => {
+  const [scope, setScope] = useState(undefined);
+  useEffect(() => {
+    function getStoredScope() {
+      const scopeStored = localStorage.getItem("scope");
+      if (scopeStored !== undefined && scopeStored !== null) {
+        const scope = JSON.parse(scopeStored);
+        console.log("setting scope - FUNCTION");
+        setStoredScope(scope);
+      }
     }
+    getStoredScope();
+  }, []);
+  const setStoredScope = (newScope) => {
+    localStorage.setItem("scope", JSON.stringify(newScope));
+    setScope(newScope);
   };
-  setScope = (scope) => {
-    localStorage.setItem("scope", JSON.stringify(scope));
-    this.setState({ scope });
-  };
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <SWRConfig
-        value={{
-          refreshInterval: 0,
-          fetcher: (...args) => fetch(...args).then((res) => res.json()),
-        }}
-      >
-        <ScopeContext.Provider
-          value={{
-            scope: this.state.scope,
-            setScope: this.setScope,
-          }}
-        >
-          <Component {...pageProps} />
-        </ScopeContext.Provider>
-      </SWRConfig>
-    );
-  }
-}
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 0,
+        fetcher: (...args) => fetch(...args).then((res) => res.json()),
+      }}
+    >
+      <ScopeContext.Provider value={{ scope, setScope: setStoredScope }}>
+        <Component {...pageProps} />
+      </ScopeContext.Provider>
+    </SWRConfig>
+  );
+};
 
-// import { useEffect, useState } from "react";
-// import ScopeContext from "../context/scopeContext";
-// import { SWRConfig } from "swr";
-
-// import "../styles/global.css";
-
-// const App = ({ Component, pageProps }) => {
-//   const [scope, setScope] = useState(undefined);
-//   useEffect(() => {
-//     function getStoredScope() {
-//       const scopeStored = localStorage.getItem("scope");
-//       if (scopeStored !== undefined && scopeStored !== null) {
-//         const scope = JSON.parse(scopeStored);
-//         console.log("setting scope - FUNCTION");
-//         setStoredScope(scope);
-//       }
-//     }
-//     getStoredScope();
-//   }, []);
-//   const setStoredScope = (newScope) => {
-//     localStorage.setItem("scope", JSON.stringify(newScope));
-//     setScope(newScope);
-//   };
-//   return (
-//     <SWRConfig
-//       value={{
-//         refreshInterval: 0,
-//         fetcher: (...args) => fetch(...args).then((res) => res.json()),
-//       }}
-//     >
-//       <ScopeContext.Provider
-//         value={{
-//           scope,
-//           setScope,
-//         }}
-//       >
-//         <Component {...pageProps} />
-//       </ScopeContext.Provider>
-//     </SWRConfig>
-//   );
-// };
-
-// export default App;
+export default App;
