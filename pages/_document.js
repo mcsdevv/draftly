b1cc2929b46a1212a1efaba2c6052e881bfda672
msweeneydev <mail@msweeneydev.com>
2020-05-07 20:42:11 +0100

    minor

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,12 @@ export default class MyDocument extends Document {
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Heebo&display=swap"
+            rel="preload"
+            as="style"
+          />
+          {/* <link rel="preconnect" href="https://fonts.gstatic.com/"> */}
+          <link
+            href="https://fonts.googleapis.com/css2?family=Heebo&display=swap"
             rel="stylesheet"
           />
         </Head>
