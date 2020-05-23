1ac7c536de1e1337c6a391fe5e7f688b624a817e
msweeneydev <mail@msweeneydev.com>
2020-05-24 00:39:35 +0100

    try postgres

---
+++ pages/api/_util/db/index.js
@@ -0,0 +1,15 @@
+const { Client } = require("pg");
+
+export const query = async (text, params, callback) => {
+  const db = new Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
+  await db.connect();
+  const res = await db.query(text, params, callback);
+  await db.end();
+  return res;
+};
