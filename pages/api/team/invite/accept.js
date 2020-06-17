2309fa589f30b287c706b41bcdf158fa04c976f7
msweeneydev <mail@msweeneydev.com>
2020-06-17 13:10:58 +0100

    path fixes

--- pages/api/team/invite/accept.js
+++ pages/api/team/invite/accept.js
@@ -1,7 +1,7 @@
 import cookie from "cookie";
 import cookieOptions from "../../_util/cookie/options";
 import { escape, query } from "@lib/api/db";
-import createInviteCode from "@lib/client/createInviteCode";
+import createInviteCode from "@lib/api/createInviteCode";
 
 const acceptInvite = async (req, res) => {
   const { id_token, uid } = req.cookies;
