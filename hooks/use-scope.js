f41e8b7598a386facacbffe7dfd1b21e64427b57
msweeneydev <mail@msweeneydev.com>
2020-06-19 11:33:06 +0100

    fix fetcher call for scope

--- hooks/use-scope.js
+++ hooks/use-scope.js
@@ -4,14 +4,17 @@ import useSWR from "swr";
 import { useUser } from "./";
 
 export const useScope = () => {
-  const { data: scope, mutate: setScope } = useSWR("scope", { initiaData: "" });
+  const { data: scope, mutate: setScope } = useSWR("scope", {
+    fetcher: undefined,
+    initiaData: "",
+  });
   const { teams } = useUser();
   useEffect(() => {
     function setNewScope() {
       if (teams) {
         const newScope = teams[0];
         Cookies.set("tuid", newScope.tuid);
-        setScope(newScope);
+        setScope("scope", newScope);
       }
     }
     setNewScope();
