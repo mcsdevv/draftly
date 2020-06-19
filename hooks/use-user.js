0d7bfcceaf00192747ad19676dad5cb5d9dec995
msweeneydev <mail@msweeneydev.com>
2020-06-19 12:41:14 +0100

    use token for user fetch dependency

--- hooks/use-user.js
+++ hooks/use-user.js
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import Cookies from "js-cookie";
 
 export const useUser = () => {
-  const token = Cookies.get("access_token");
+  const token = Cookies.get("id_token");
   const { data, revalidate } = useSWR(token ? `/api/user` : null);
   return { ...data, revalidateProfile: revalidate };
 };
