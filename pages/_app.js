38ecc4961d08ecb5928dccbbf3219c3a6a227ec7
msweeneydev <mail@msweeneydev.com>
2020-06-24 22:53:39 +0100

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,15 @@
 import { SWRConfig } from "swr";
 import "@styles/global.css";
 
-const App = ({ Component, pageProps }) => {
-  return (
-    <SWRConfig
-      value={{
-        refreshInterval: 0,
-        fetcher: (...args) => fetch(...args).then((res) => res.json()),
-      }}
-    >
-      <Component {...pageProps} />
-    </SWRConfig>
-  );
-};
+const App = ({ Component, pageProps }) => (
+  <SWRConfig
+    value={{
+      refreshInterval: 0,
+      fetcher: (...args) => fetch(...args).then((res) => res.json()),
+    }}
+  >
+    <Component {...pageProps} />
+  </SWRConfig>
+);
 
 export default App;
