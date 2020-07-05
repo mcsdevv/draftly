4f3a0bde4d3796a7eee20db110d5a24ebd038b2b
mcsdevv <mail@mcs.dev>
2020-07-05 22:30:18 +0100

    removal isValidating

--- hooks/use-tweets.ts
+++ hooks/use-tweets.ts
@@ -6,12 +6,9 @@ export default function useTweets() {
   const { scope } = useScope();
   const tuid = scope?.tuid;
 
-  const {
-    data,
-    isValidating,
-    revalidate: revalidateTweets,
-    mutate: setTweets,
-  } = useSWR(tuid ? `/api/tweets` : null);
+  const { data, revalidate: revalidateTweets, mutate: setTweets } = useSWR(
+    tuid ? `/api/tweets` : null
+  );
 
-  return { ...data, isValidating, revalidateTweets, setTweets };
+  return { ...data, revalidateTweets, setTweets };
 }
--- pages/[handle]/tweets/drafts/index.js
+++ pages/[handle]/tweets/drafts/index.js
@@ -15,27 +15,26 @@ import DashboardLayout from "@components/layouts/dashboard";
 
 function Drafts() {
   const { scope } = useScope();
-  const { drafts, isValidating } = useTweets();
+  const { drafts } = useTweets();
 
   return (
     <Container size={2}>
       <Table headers={["Text", "Created By", "Created At", "Last Updated"]}>
-        {!isValidating &&
-          drafts?.map((d) => (
-            <Row
-              key={d.twuid}
-              row={[
-                d.text,
-                [...scope?.members, ...scope?.owners].find(
-                  (m) => m.uid === d.created_by
-                )?.name,
-                ago(new Date(d.created_at)),
-                ago(new Date(d.updated_at)),
-              ]}
-              twuid={d.twuid}
-              type="drafts"
-            />
-          ))}
+        {drafts?.map((d) => (
+          <Row
+            key={d.twuid}
+            row={[
+              d.text,
+              [...scope?.members, ...scope?.owners].find(
+                (m) => m.uid === d.created_by
+              )?.name,
+              ago(new Date(d.created_at)),
+              ago(new Date(d.updated_at)),
+            ]}
+            twuid={d.twuid}
+            type="drafts"
+          />
+        ))}
       </Table>
     </Container>
   );
