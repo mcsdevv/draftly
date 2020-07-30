c719edeed87d3bf4c1a5426d96ef214b5f9522aa
mcsdevv <mail@mcs.dev>
2020-07-31 00:59:20 +0100

    error test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 // * Components
 import DashboardLayout from "@components/layouts/dashboard";
 
-const Landing = () => <h1>Draft & Publish</h1>;
+const Landing = () => <><h1>Draft & Publish</h1><button onClick={()=> throw new Error}>Throw Error</button></>;
 
 Landing.getLayout = (page: React.ReactNode) => (
   <DashboardLayout name="Landing">{page}</DashboardLayout>
