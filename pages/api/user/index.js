047aea2043cfe47b30ed9c62e9b69f23219ce51d
mcsdevv <mail@mcs.dev>
2020-07-31 12:07:44 +0100

    remove try catch at endpoint level

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -3,60 +3,55 @@ import withSentry from "@lib/api/middleware/withSentry";
 import { escape, query } from "@lib/api/db";
 
 const getUserDetails = async (req, res, uid) => {
-  try {
-    throw "derps";
-    // * Select user from database
-    const [userQuery] = await query(
-      escape`SELECT * FROM users WHERE uid = ${uid}`
-    );
+  throw "derps";
+  // * Select user from database
+  const [userQuery] = await query(
+    escape`SELECT * FROM users WHERE uid = ${uid}`
+  );
 
-    // * Select all teams of the user
-    const teams = await query(
-      escape`SELECT * FROM teams_members
+  // * Select all teams of the user
+  const teams = await query(
+    escape`SELECT * FROM teams_members
       LEFT JOIN teams ON teams.tuid = teams_members.tuid
       WHERE uid = ${uid}`
-    );
+  );
 
-    // * Delete keys for all teams
-    const teamsNoKeys = teams.map((t) => {
-      const { token_key, token_secret, ...team } = t;
-      return team;
-    });
+  // * Delete keys for all teams
+  const teamsNoKeys = teams.map((t) => {
+    const { token_key, token_secret, ...team } = t;
+    return team;
+  });
 
-    // * Get all team members for listed teams
-    const getTeamMembers = async () => {
-      const members = await Promise.all(
-        teamsNoKeys.map((t) => {
-          return query(
-            escape`SELECT * FROM teams_members
+  // * Get all team members for listed teams
+  const getTeamMembers = async () => {
+    const members = await Promise.all(
+      teamsNoKeys.map((t) => {
+        return query(
+          escape`SELECT * FROM teams_members
             LEFT JOIN users ON users.uid = teams_members.uid
             WHERE tuid = ${t.tuid}`
-          );
-        })
-      );
-      const membersFlat = [].concat.apply([], members);
-      return membersFlat;
+        );
+      })
+    );
+    const membersFlat = [].concat.apply([], members);
+    return membersFlat;
+  };
+
+  const teamMembers = await getTeamMembers();
+
+  // * Add members to all teams
+  const teamsWithMembers = teamsNoKeys.map((t) => {
+    const members = teamMembers.filter((m) => m.tuid === t.tuid);
+    return {
+      ...t,
+      members: members.filter((m) => m.role === "member"),
+      owners: members.filter((m) => m.role === "owner"),
     };
+  });
 
-    const teamMembers = await getTeamMembers();
-
-    // * Add members to all teams
-    const teamsWithMembers = teamsNoKeys.map((t) => {
-      const members = teamMembers.filter((m) => m.tuid === t.tuid);
-      return {
-        ...t,
-        members: members.filter((m) => m.role === "member"),
-        owners: members.filter((m) => m.role === "owner"),
-      };
-    });
-
-    console.log("User details:", userQuery);
-    console.log("Teams details:", teamsWithMembers);
-    res.status(200).json({ user: userQuery, teams: teamsWithMembers });
-  } catch (err) {
-    console.error("ERROR - api/user -", err.message);
-    res.status(500).json({ err: err.message });
-  }
+  console.log("User details:", userQuery);
+  console.log("Teams details:", teamsWithMembers);
+  res.status(200).json({ user: userQuery, teams: teamsWithMembers });
 };
 
 export default verify(withSentry(getUserDetails));
