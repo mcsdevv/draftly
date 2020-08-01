e5ee941a7d3eabc93958f37f9c733f34ea8a5473
mcsdevv <mail@mcs.dev>
2020-08-01 20:02:43 +0100

    update controls

--- components/controls/index.tsx
+++ components/controls/index.tsx
@@ -1,5 +1,14 @@
 // * Modulz
-import { Button, Card, Flex } from "@modulz/radix";
+import {
+  Button,
+  Card,
+  Divider,
+  Flex,
+  Heading,
+  Option,
+  Select,
+  Subheading,
+} from "@modulz/radix";
 
 interface ControlsProps {
   approvals?: number;
@@ -30,48 +39,69 @@ const Controls = ({
   handleUpdate,
 }: ControlsProps) => (
   <Card sx={{ width: "100%" }} mt={4}>
+    <Heading as="h2" size={4}>
+      Controls
+    </Heading>
+    <Divider mb={2} />
     <Flex
       sx={{
-        alignContent: "center",
-        justifyContent: "center",
-        padding: "16px 0",
+        paddingTop: "16px",
       }}
     >
-      <Button
-        sx={{ cursor: "pointer" }}
-        onClick={!editing ? handleDelete : handleCancelEdit}
-        variant="red"
-      >
-        {!editing ? "Delete" : "Cancel"}
-      </Button>
-      <Button
-        sx={{ cursor: "pointer" }}
-        onClick={!editing ? handleEdit : handleUpdate}
-        ml={4}
-      >
-        {!editing ? "Edit" : "Update"}
-      </Button>
-      <Button
-        sx={{ cursor: "pointer" }}
-        disabled={disableApprove}
-        ml={4}
-        onClick={handleApprove}
-        variant="green"
-      >
-        {approvalsRequired &&
-          approvalsRequired > 0 &&
-          `${approvals} / ${approvalsRequired}`}{" "}
-        Approve
-      </Button>
-      <Button
-        sx={{ cursor: "pointer" }}
-        disabled={disablePublish}
-        onClick={handlePublish}
-        ml={4}
-        variant="blue"
-      >
-        Publish
-      </Button>
+      <Flex mr={4} sx={{ flexDirection: "column" }}>
+        <Subheading mb={5}>Select Theme</Subheading>
+        <Select value="default" sx={{ width: "120px" }}>
+          <Option label="Default" value="default" />
+          <Option label="Dim" value="dim" />
+          <Option label="Lights Out" value="lights-out" />
+        </Select>
+      </Flex>
+      <Flex mr={4} sx={{ flexDirection: "column" }}>
+        <Button mb={4} sx={{ cursor: "pointer", width: "120px" }}>
+          Request Feedback
+        </Button>
+        <Button sx={{ cursor: "pointer", width: "120px" }}>
+          Refresh Metadata
+        </Button>
+      </Flex>
+      <Flex mr={4} sx={{ flexDirection: "column" }}>
+        <Button
+          mb={4}
+          sx={{ cursor: "pointer", width: "120px" }}
+          onClick={!editing ? handleDelete : handleCancelEdit}
+          variant="red"
+        >
+          {!editing ? "Delete" : "Cancel"}
+        </Button>
+        <Button
+          sx={{ cursor: "pointer", width: "120px" }}
+          onClick={!editing ? handleEdit : handleUpdate}
+        >
+          {!editing ? "Edit" : "Update"}
+        </Button>
+      </Flex>
+      <Flex sx={{ flexDirection: "column" }}>
+        <Button
+          sx={{ cursor: "pointer", width: "120px" }}
+          disabled={disableApprove}
+          mb={4}
+          onClick={handleApprove}
+          variant="green"
+        >
+          {approvalsRequired &&
+            approvalsRequired > 0 &&
+            `${approvals} / ${approvalsRequired}`}{" "}
+          Approve
+        </Button>
+        <Button
+          sx={{ cursor: "pointer" }}
+          disabled={disablePublish}
+          onClick={handlePublish}
+          variant="blue"
+        >
+          Publish
+        </Button>
+      </Flex>
     </Flex>
   </Card>
 );
