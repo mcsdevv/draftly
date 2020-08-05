0ef73589ad442f278e56ba3ed436199d50cb9dcb
mcsdevv <mail@mcs.dev>
2020-08-05 01:03:49 +0100

    delete published tweets

---
+++ pages/api/tweet/published/delete.js
@@ -0,0 +1,18 @@
+import verify from "@lib/api/token/verify";
+import withSentry from "@lib/api/middleware/withSentry";
+import { escape, query } from "@lib/api/db";
+
+const deletePublishedTweet = async (req, res) => {
+  const { twuid } = JSON.parse(req.body);
+
+  // * Delete published tweet
+  await query(escape`DELETE FROM tweets WHERE twuid = ${twuid}`);
+
+  // * Delete published tweet meta
+  await query(escape`DELETE FROM tweets_meta WHERE twuid = ${twuid}`);
+
+  console.log("Deleted tweet:", twuid);
+  res.status(200).json(twuid);
+};
+
+export default verify(withSentry(deletePublishedTweet));
