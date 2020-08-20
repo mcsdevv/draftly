ad4f741e4820cf348053255aa0a2bf02aa520652
mcsdevv <mail@mcs.dev>
2020-08-20 21:55:19 +0100

    added invite code invalid error page

--- pages/errors/invite-code-invalid.tsx
+++ pages/errors/invite-code-invalid.tsx
@@ -2,7 +2,7 @@
 import { useRouter } from "next/router";
 
 // * Modulz
-import { Divider, Heading, Text } from "@modulz/radix";
+import { Container, Divider, Heading, Text } from "@modulz/radix";
 
 // * Components
 import MarketingLayout from "@components/layouts/marketing";
@@ -11,20 +11,22 @@ function InviteCodeInvalid() {
   const router = useRouter();
   const team = router.query?.team;
   return (
-    <>
-      <Heading>Error: Invite Code Invalid</Heading>
-      <Divider />
+    <Container size={1}>
+      <Heading as="h1" mb={2} mt={4} size={4}>
+        Invite Code Invalid
+      </Heading>
+      <Divider mb={2} />
       <Text>
         The invite code{" "}
         {team ? `provided to join the ${team} team` : "provided"} has expired,
         please request a new invite from a team owner.
       </Text>
-    </>
+    </Container>
   );
 }
 
 InviteCodeInvalid.getLayout = (page: React.ReactNode) => (
-  <MarketingLayout name="Landing">{page}</MarketingLayout>
+  <MarketingLayout name="Error">{page}</MarketingLayout>
 );
 
 export default InviteCodeInvalid;
