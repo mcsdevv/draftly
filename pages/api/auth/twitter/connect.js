8aac8cbdb95bb2faf45cf81a89dce786a1422043
mcsdevv <mail@mcs.dev>
2020-08-22 23:25:05 +0100

    prevent caching for twitter account connect

--- pages/api/auth/twitter/connect.js
+++ pages/api/auth/twitter/connect.js
@@ -2,7 +2,6 @@ import oauth from "@lib/api/oauth";
 import withSentry from "@lib/api/middleware/withSentry";
 
 const twitterConnect = (_req, res) => {
-  // TODO Stop request from getting cached, causes 'old request token' error.
   oauth.getOAuthRequestToken(function (error, oauthToken, oauthTokenSecret) {
     // * Check for an error receiving request tokens
     if (error) {
@@ -12,6 +11,7 @@ const twitterConnect = (_req, res) => {
     } else {
       // * Redirect to authorization endpoint to get access tokens
       res.writeHead(301, {
+        "Cache-Control": "no-store",
         Location: `https://api.twitter.com/oauth/authorize?oauth_token=${oauthToken}`,
       });
       res.end();
