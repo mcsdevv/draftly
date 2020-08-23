081428bdf2ddc558caefb74e16c27508f962bf1a
mcsdevv <mail@mcs.dev>
2020-08-23 12:19:57 +0100

    test

--- components/layouts/pages/marketing/index.tsx
+++ components/layouts/pages/marketing/index.tsx
@@ -6,7 +6,7 @@ import { Container, Divider, Flex, Heading } from "@modulz/radix";
 
 // * Components
 import Header from "@components/header/marketing";
-import RequireLogin from "@lib/client/requireLogin";
+// import RequireLogin from "@lib/client/requireLogin";
 
 interface MarketingLayoutProps {
   children: React.ReactNode;
@@ -24,22 +24,22 @@ const MarketingLayout = ({ children, name }: MarketingLayoutProps) => {
   }, []);
 
   return loaded ? (
-    <RequireLogin>
-      <Container
-        sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
-        size={3}
-      >
-        <Header />
-        <Flex sx={{ justifyContent: "space-between" }}>
-          <Heading size={5} weight="medium">
-            {name}
-          </Heading>
-        </Flex>
-        <Divider mb={4} />
-        <Flex sx={{ flexGrow: 1 }}>{children}</Flex>
-      </Container>
-    </RequireLogin>
-  ) : null;
+    // <RequireLogin>
+    <Container
+      sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+      size={3}
+    >
+      <Header />
+      <Flex sx={{ justifyContent: "space-between" }}>
+        <Heading size={5} weight="medium">
+          {name}
+        </Heading>
+      </Flex>
+      <Divider mb={4} />
+      <Flex sx={{ flexGrow: 1 }}>{children}</Flex>
+    </Container>
+  ) : // </RequireLogin>
+  null;
 };
 
 export default MarketingLayout;
