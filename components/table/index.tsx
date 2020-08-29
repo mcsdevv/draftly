0bbe052232b2be06fbe214f1477dad935c45a222
mcsdevv <mail@mcs.dev>
2020-08-29 21:33:54 +0100

    fix table pagination

--- components/table/index.tsx
+++ components/table/index.tsx
@@ -14,8 +14,8 @@ interface TableProps {
   children: React.ReactNode;
   handleNextPage?: () => void;
   handlePreviousPage?: () => void;
-  pageNumber?: number;
-  pageMax?: number;
+  pageNumber: number;
+  pageMax: number;
 }
 
 const Table = ({
@@ -47,17 +47,31 @@ const Table = ({
         justifyContent: "space-between",
       }}
     >
-      <IconButton sx={{ cursor: "pointer" }} onClick={handlePreviousPage}>
-        <ArrowLeftIcon />
-        <Text ml={1}>Previous Page</Text>
-      </IconButton>
-      <Text>
-        Page {pageNumber} of {pageMax}
-      </Text>
-      <IconButton sx={{ cursor: "pointer" }} onClick={handleNextPage}>
-        <Text mr={1}>Next Page</Text>
-        <ArrowRightIcon />
-      </IconButton>
+      <Flex sx={{ justifyContent: "flex-start", width: "128px" }}>
+        {pageNumber > 1 && (
+          <>
+            <IconButton sx={{ cursor: "pointer" }} onClick={handlePreviousPage}>
+              <ArrowLeftIcon />
+            </IconButton>
+            <Text ml={1}>Previous Page</Text>
+          </>
+        )}
+      </Flex>
+      <Flex sx={{ justifyContent: "center", width: "128px" }}>
+        <Text>
+          Page {pageNumber} of {pageMax}
+        </Text>
+      </Flex>
+      <Flex sx={{ justifyContent: "flex-end", width: "128px" }}>
+        {pageNumber < pageMax && (
+          <>
+            <Text mr={1}>Next Page</Text>
+            <IconButton sx={{ cursor: "pointer" }} onClick={handleNextPage}>
+              <ArrowRightIcon />
+            </IconButton>
+          </>
+        )}
+      </Flex>
     </Flex>
   </>
 );
