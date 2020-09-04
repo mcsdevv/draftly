26e443672af496f244a22bec55fedfef80fe74c5
mcsdevv <mail@mcs.dev>
2020-09-05 00:38:48 +0100

    test

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -9,24 +9,24 @@ const login = async (req, res) => {
   const state = uuidv4();
   const nonce = uuidv4();
 
+  const redirect_uri = getRedirectUrl(req, res);
+
   // * Add state and nonce as httpOnly cookie for callback to check
   res.setHeader("Set-Cookie", [
     cookie.serialize("state", String(state), cookieOptions(true, false)),
     cookie.serialize("nonce", String(nonce), cookieOptions(true, false)),
+    cookie.serialize(
+      "redirect_uri",
+      String(redirect_uri || ""),
+      cookieOptions(true, false)
+    ),
   ]);
 
   console.log("INIT STATE", state);
 
   // * Write redirect including openid, profile, and email scopes
   res.writeHead(302, {
-    Location: `https://${
-      process.env.AUTH0_DOMAIN
-    }/authorize?response_type=code&audience=${
-      process.env.AUTH0_AUDIENCE
-    }&client_id=${process.env.AUTH0_CLIENT_ID}&redirect_uri=${getRedirectUrl(
-      req,
-      res
-    )}/api/auth/callback&scope=openid%20profile%20email%20&state=${state}&nonce=${nonce}`,
+    Location: `https://${process.env.AUTH0_DOMAIN}/authorize?response_type=code&audience=${process.env.AUTH0_AUDIENCE}&client_id=${process.env.AUTH0_CLIENT_ID}&redirect_uri=${redirect_uri}/api/auth/callback&scope=openid%20profile%20email%20&state=${state}&nonce=${nonce}`,
   });
   res.end();
 };
