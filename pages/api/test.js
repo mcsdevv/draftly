5c678dcbcf9d140e290fdcbd7cf369d4a37f8920
mcsdevv <mail@mcs.dev>
2020-09-24 00:55:40 +0100

    tests

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,14 +3,29 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handle(req, res) {
-  const tweets = await prisma.tweets.findMany({
-    where: { tuid: "40f2df95-6488-4b7b-8d82-46744f548489" },
-    include: {
-      tweets_approvals: true,
-      tweets_comments: true,
-      tweets_meta: true,
+  prisma.$use(async (params, next, uid, tuid) => {
+    console.log(params);
+    console.log("1");
+    const result = await next(params);
+    console.log("4");
+    return result;
+  });
+  // const tweets = await prisma.tweets.findMany({
+  //   where: { tuid: "0afb2e30-a728-436f-9b40-6d6cb47fd22b" },
+  //   include: {
+  //     tweets_approvals: true,
+  //     tweets_comments: true,
+  //     tweets_meta: true,
+  //   },
+  // });
+  const isOwner = await prisma.teams_members.findOne({
+    where: {
+      uid_tuid: {
+        uid: "490134e6-7231-4660-9560-5e7c1cf65186",
+        tuid: "0afb2e30-a728-436f-9b40-6d6cb47fd22b",
+      },
     },
   });
+  console.log("OWNER", isOwner);
   res.json(tweets);
-  // res.send(process.env.DATABASE_URL);
 }
