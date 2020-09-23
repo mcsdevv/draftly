0f043a1a540039ff7419fe87dc7179cb52bea230
mcsdevv <mail@mcs.dev>
2020-09-23 01:47:09 +0100

    test

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,24 +1,15 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 export default async function handle(req, res) {
-  console.log("pw", process.env.MYSQL_PASSWORD);
-  console.log("user", process.env.MYSQL_USER);
-  console.log("db", process.env.MYSQL_DATABASE);
-  console.log("host", process.env.MYSQL_HOST);
-  // const tweets = await prisma.tweets.findMany({
-  //   where: { tuid: "0afb2e30-a728-436f-9b40-6d6cb47fd22b" },
-  //   include: {
-  //     tweets_approvals: true,
-  //     tweets_comments: true,
-  //     tweets_meta: true,
-  //   },
-  // });
-  res.json({
-    pw: process.env.MYSQL_PASSWORD,
-    username: process.env.MYSQL_USER,
-    db: process.env.MYSQL_DATABASE,
-    host: process.env.MYSQL_HOST,
+  const tweets = await prisma.tweets.findMany({
+    where: { tuid: "0afb2e30-a728-436f-9b40-6d6cb47fd22b" },
+    include: {
+      tweets_approvals: true,
+      tweets_comments: true,
+      tweets_meta: true,
+    },
   });
+  res.json(tweets);
 }
