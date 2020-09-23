44105e0dd24a12ce15738968ee9ae4091f3b8743
mcsdevv <mail@mcs.dev>
2020-09-23 01:59:04 +0100

    test

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,14 +3,14 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handle(req, res) {
-  // const tweets = await prisma.tweets.findMany({
-  //   where: { tuid: "0afb2e30-a728-436f-9b40-6d6cb47fd22b" },
-  //   include: {
-  //     tweets_approvals: true,
-  //     tweets_comments: true,
-  //     tweets_meta: true,
-  //   },
-  // });
-  // res.json(tweets);
-  res.send(process.env.DATABASE_URL);
+  const tweets = await prisma.tweets.findMany({
+    where: { tuid: "0afb2e30-a728-436f-9b40-6d6cb47fd22b" },
+    include: {
+      tweets_approvals: true,
+      tweets_comments: true,
+      tweets_meta: true,
+    },
+  });
+  res.json(tweets);
+  // res.send(process.env.DATABASE_URL);
 }
