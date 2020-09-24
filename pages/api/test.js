01b3ba453482951ae76ed41495f4378dc6b7ba22
mcsdevv <mail@mcs.dev>
2020-09-24 23:05:00 +0100

    migration test

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,30 +2,26 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default async function handle(req, res) {
-  prisma.$use(async (params, next, uid, tuid) => {
-    console.log(params);
-    console.log("1");
-    const result = await next(params);
-    console.log("4");
-    return result;
-  });
-  // const tweets = await prisma.tweets.findMany({
-  //   where: { tuid: "0afb2e30-a728-436f-9b40-6d6cb47fd22b" },
-  //   include: {
-  //     tweets_approvals: true,
-  //     tweets_comments: true,
-  //     tweets_meta: true,
-  //   },
-  // });
-  const isOwner = await prisma.teams_members.findOne({
-    where: {
-      uid_tuid: {
-        uid: "490134e6-7231-4660-9560-5e7c1cf65186",
-        tuid: "0afb2e30-a728-436f-9b40-6d6cb47fd22b",
-      },
+const getTweets = async (req, res, uid, tuid) => {
+  const tweets = await prisma.tweets.findMany({
+    where: { tuid },
+    include: {
+      tweets_approvals: true,
+      tweets_comments: true,
+      tweets_meta: true,
     },
   });
-  console.log("OWNER", isOwner);
+
   res.json(tweets);
-}
+};
+
+export default getTweets;
+
+// const isOwner = await prisma.teams_members.findOne({
+//   where: {
+//     uid_tuid: {
+//       uid: "490134e6-7231-4660-9560-5e7c1cf65186",
+//       tuid: "0afb2e30-a728-436f-9b40-6d6cb47fd22b",
+//     },
+//   },
+// });
--- pages/api/tweets/index.js
+++ pages/api/tweets/index.js
@@ -1,70 +1,19 @@
 import verify from "@lib/api/token/verify";
 import withSentry from "@lib/api/middleware/withSentry";
 import { escape, query } from "@lib/api/db";
-import isMember from "@lib/api/middleware/isMember";
+import isMember from "@lib/api/middleware/isMember2";
 
 const getAllTweets = async (req, res, _uid, tuid) => {
   const { draftLimit, draftPage, publishedLimit, publishedPage } = req.query;
-  // * Get all tweets for team
-  console.time("getTweets");
-  const tweetsQuery = await query(
-    escape`SELECT * FROM tweets
-      WHERE tuid = ${tuid}
-      ORDER BY updated_at DESC`
-  );
-  console.timeEnd("getTweets");
 
-  // * Get all tweet metadata
-  console.time("getMeta");
-  const metaQuery = await query(
-    escape`SELECT * FROM tweets_meta
-      LEFT JOIN tweets ON tweets.twuid = tweets_meta.twuid
-      WHERE tuid = ${tuid}`
-  );
-  console.timeEnd("getMeta");
-
-  // * Get all tweet approvals
-  const approvalsQuery = async () => {
-    const approvals = await Promise.all(
-      tweetsQuery.map((t) => {
-        return query(
-          escape`SELECT * FROM tweets_approvals
-            WHERE twuid = ${t.twuid}`
-        );
-      })
-    );
-    return approvals;
-  };
-
-  // * Flatten approvals array
-  console.time("getApprovals");
-  const tweetApprovals = await approvalsQuery();
-  console.timeEnd("getApprovals");
-  console.time("flattenApprovals");
-  const approvals = [].concat.apply([], tweetApprovals);
-  console.timeEnd("flattenApprovals");
-
-  // * Get all tweet comments
-  const commentsQuery = async () => {
-    const comments = await Promise.all(
-      tweetsQuery.map((t) => {
-        return query(
-          escape`SELECT * FROM tweets_comments
-            WHERE twuid = ${t.twuid}
-            ORDER BY added_at DESC`
-        );
-      })
-    );
-    return comments;
-  };
-
-  // * Flatten comments array
-  console.time("getComments");
-  const tweetComments = await commentsQuery();
-  console.timeEnd("getComments");
-  console.time("flattenComments");
-  const comments = [].concat.apply([], tweetComments);
-  console.timeEnd("flattenComments");
+  const tweets = await prisma.tweets.findMany({
+    where: { tuid },
+    include: {
+      tweets_approvals: true,
+      tweets_comments: true,
+      tweets_meta: true,
+    },
+  });
 
   // * Add meta, approvals, and comments to tweets
   const tweets = tweetsQuery.map((t) => {
