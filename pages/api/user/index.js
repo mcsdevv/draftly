ab3c3ea7daad4e56392a9d15563dd819d5791c0a
mcsdevv <mail@mcs.dev>
2020-09-26 23:42:51 +0100

    prisma for user get

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -1,87 +1,46 @@
-import verify from "@lib/api/token/verify";
-import withSentry from "@lib/api/middleware/withSentry";
-import { escape, query } from "@lib/api/db";
-
 // * Libraries
 import { PrismaClient } from "@prisma/client";
 
-// // * Middleware
-// import verify from "@lib/api/token/verify";
-// import withSentry from "@lib/api/middleware/withSentry";
+// * Middleware
+import verify from "@lib/api/token/verify";
+import withSentry from "@lib/api/middleware/withSentry";
 
 // * Initialize Prisma client outside of handler
 const prisma = new PrismaClient();
 
 const getUserDetails = async (_req, res, uid) => {
-  // * Select user, teams part of, and team members
-  const data = await query(
-    escape`SELECT * FROM users WHERE uid = ${uid};
-
-    SELECT * FROM teams t
-    LEFT JOIN teams_members tm ON t.tuid = tm.tuid
-    WHERE uid = ${uid};
-
-    SELECT * FROM users u
-    LEFT JOIN teams_members tm ON u.uid = tm.uid
-    WHERE tm.tuid in(
-      SELECT t.tuid 
-      FROM teams t
-      LEFT JOIN teams_members tm ON t.tuid = tm.tuid
-      WHERE uid = ${uid}
-    );`
-  );
-
-  const profile = await prisma.teams_members.findMany({
+  // * Select user with matching uid
+  const user = await prisma.users.findOne({
     where: { uid },
-    select: {
-      teams: {
-        include: {
-          members: true,
-        },
-      },
-      user: {
-        where: { uid },
+  });
+
+  // * Select teams data that will require filtering
+  const teamsData = await prisma.teams.findMany({
+    where: { members: { some: { uid } } },
+    include: {
+      members: {
+        include: { user: true },
       },
     },
   });
 
-  // const profile = await prisma.users
-  //   .findOne({
-  //     where: { uid },
-  //   })
-  //   .teams_members({
-  //     select: {
-  //       teams: {
-  //         include: {
-  //           members: true,
-  //         },
-  //       },
-  //       user: true,
-  //     },
-  //   });
+  const teams = teamsData.map((t) => {
+    // * Remove tokens that are server only
+    delete t.token_key;
+    delete t.token_secret;
 
-  const allData = () => {
-    const user = { ...data[0] };
-
-    const teams = data[1].map((t) => {
-      // TODO Request less from database
-      delete t.role;
-      delete t.token_key;
-      delete t.token_secret;
-      delete t.uid;
-      return {
-        ...t,
-        members: data[2].filter(
-          (m) => m.role === "member" && m.tuid === t.tuid
-        ),
-        owners: data[2].filter((m) => m.role === "owner" && m.tuid === t.tuid),
-      };
-    });
-    return { user, teams };
-  };
+    // * Separate members and owners
+    const members = t.members.filter((m) => m.role === "member");
+    const owners = t.members.filter((m) => m.role === "owner");
+    return {
+      ...t,
+      members,
+      owners,
+    };
+  });
 
   console.log("Retrieved user details for:", uid);
-  res.status(200).json({ ...allData(), profile });
+  res.status(200).json({ user, teams });
 };
 
 export default verify(withSentry(getUserDetails));
