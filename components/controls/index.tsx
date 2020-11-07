f004f97117692a3c6c0fcb01e2eacaaa3f4fe83f
mcsdevv <mail@mcs.dev>
2020-11-07 17:15:37 +0000

    owner only for tweet deletion

--- components/controls/index.tsx
+++ components/controls/index.tsx
@@ -50,25 +50,77 @@ const Controls = ({
     // * Disable approval if user is creator
     if (tweet?.createdBy === user?.uid) {
       return {
+        disabled: false,
         label: "You cannot approve your own tweet.",
-        status: false,
       };
     }
 
     // * Disable approval if user has already approved
     if (tweet?.approvals.find((a: any) => a.uid === user?.uid)) {
       return {
+        disabled: false,
         label: "You have already approved this tweet.",
-        status: false,
       };
     }
 
     // * Enable approval
     return {
+      disabled: true,
       label: "Click to approve this tweet.",
-      status: true,
     };
   }, [scope, tweet]);
+
+  // * Calculate the correct edit state
+  const deleteStatus = useMemo(() => {
+    // * Find user as owner within the team
+    const isOwner = scope.owners.find((o: any) => o.uid === user.uid);
+
+    // * Default state, not editing but owner
+    if (!editing && isOwner) {
+      return {
+        disabled: !isOwner,
+        label: "Click to delete this tweet.",
+        onClick: handleDelete,
+        text: "Delete",
+      };
+    }
+
+    // * Default state, not editing but NOT owner
+    if (!editing && isOwner) {
+      return {
+        disabled: !isOwner,
+        label: "Click to delete this tweet.",
+        onClick: handleDelete,
+        text: "Delete",
+      };
+    }
+
+    // * Secondary state, already editing
+    return {
+      label: "Click to cancel your edit.",
+      onClick: handleCancelEdit,
+      text: "Cancel",
+    };
+  }, [editing]);
+
+  // * Calculate the correct edit state
+  const editStatus = useMemo(() => {
+    // * Default state, not editing
+    if (!editing) {
+      return {
+        label: "Click to edit this tweet.",
+        onClick: handleEdit,
+        text: "Edit",
+      };
+    }
+
+    // * Secondary state, already editing
+    return {
+      label: "Click to confirm your edit.",
+      onClick: handleUpdate,
+      text: "Update",
+    };
+  }, [editing]);
 
   // * Calculate the correct publish state
   const publishStatus = useMemo(() => {
@@ -80,31 +132,35 @@ const Controls = ({
     // * Disable publish if required reviews not present
     if (approvals?.length < scope?.reviewsRequired) {
       return {
+        disabled: false,
         label: "Insufficient approvals to publish.",
-        status: false,
       };
     }
 
     // * Enable publish
     return {
+      disabled: true,
       label: "Click to publish this tweet.",
-      status: true,
     };
   }, [scope, tweet]);
   return (
     <Box>
       <Flex sx={{ justifyContent: "space-between" }}>
-        <Button
-          sx={{ cursor: "pointer", width: 100 }}
-          onClick={!editing ? handleEdit : handleUpdate}
-        >
-          {!editing ? "Edit" : "Update"}
-        </Button>
+        <Tooltip label={editStatus.label} align="center">
+          <Box>
+            <Button
+              sx={{ cursor: "pointer", width: 100 }}
+              onClick={editStatus.onClick}
+            >
+              {editStatus.text}
+            </Button>
+          </Box>
+        </Tooltip>
         <Tooltip label={approvalStatus.label} align="center">
           <Box>
             <Button
               sx={{ cursor: "pointer", width: 100 }}
-              disabled={!approvalStatus.status}
+              disabled={!approvalStatus.disabled}
               onClick={handleApprove}
             >
               {approvalsRequired &&
@@ -118,20 +174,25 @@ const Controls = ({
           <Box>
             <Button
               sx={{ cursor: "pointer", width: 100 }}
-              disabled={!publishStatus.status}
+              disabled={!publishStatus.disabled}
               onClick={handlePublish}
             >
               Publish
             </Button>
           </Box>
         </Tooltip>
-        <Button
-          sx={{ cursor: "pointer", width: 100 }}
-          onClick={!editing ? handleDelete : handleCancelEdit}
-          variant="red"
-        >
-          {!editing ? "Delete" : "Cancel"}
-        </Button>
+        <Tooltip label={deleteStatus.label} align="center">
+          <Box>
+            <Button
+              sx={{ cursor: "pointer", width: 100 }}
+              disabled={deleteStatus.disabled}
+              onClick={deleteStatus.onClick}
+              variant="red"
+            >
+              {deleteStatus.text}
+            </Button>
+          </Box>
+        </Tooltip>
       </Flex>
     </Box>
   );
