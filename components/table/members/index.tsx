745770223686acc5e7eae2205ab6bcd55f0372be
mcsdevv <mail@mcs.dev>
2020-11-08 19:38:37 +0000

    empty table state

--- components/table/members/index.tsx
+++ components/table/members/index.tsx
@@ -1,46 +1,60 @@
 // * Modulz
-import { Table, Thead, Tr, Th } from "@modulz/radix";
+import { Flex, Table, Thead, Tr, Th, Text } from "@modulz/radix";
 
 // * Components
 import MembersRow from "@components/table/members/row";
 
 interface MembersTableProps {
-  children: React.ReactNode;
+  children: React.ReactNode[];
   loading: boolean;
   type: string;
 }
 
 const MembersTable = ({ children, loading, type }: MembersTableProps) => (
-  <Table cellSpacing="1" cellPadding="0">
-    <Thead>
-      <Tr>
-        <Th sx={{ width: "160px" }}>Name</Th>
-        <Th sx={{ width: "160px" }}>Email</Th>
-        <Th sx={{ width: "80px" }}>
-          {type === "member" ? "Upgrade" : "Downgrade"}
-        </Th>
-        <Th sx={{ width: "60px" }}>Remove</Th>
-      </Tr>
-    </Thead>
-    <tbody>
-      {loading ? (
-        <>
-          <MembersRow loading />
-          <MembersRow loading />
-          <MembersRow loading />
-          <MembersRow loading />
-          <MembersRow loading />
-          <MembersRow loading />
-          <MembersRow loading />
-          <MembersRow loading />
-          <MembersRow loading />
-          <MembersRow loading />
-        </>
-      ) : (
-        children
-      )}
-    </tbody>
-  </Table>
+  <>
+    <Table cellSpacing="1" cellPadding="0">
+      <Thead>
+        <Tr>
+          <Th sx={{ width: "160px" }}>Name</Th>
+          <Th sx={{ width: "160px" }}>Email</Th>
+          <Th sx={{ width: "80px" }}>
+            {type === "member" ? "Upgrade" : "Downgrade"}
+          </Th>
+          <Th sx={{ width: "60px" }}>Remove</Th>
+        </Tr>
+      </Thead>
+      <tbody>
+        {loading ? (
+          <>
+            <MembersRow loading />
+            <MembersRow loading />
+            <MembersRow loading />
+            <MembersRow loading />
+            <MembersRow loading />
+            <MembersRow loading />
+            <MembersRow loading />
+            <MembersRow loading />
+            <MembersRow loading />
+            <MembersRow loading />
+          </>
+        ) : (
+          children
+        )}
+      </tbody>
+    </Table>
+    {children.length === 0 && (
+      <Flex
+        sx={{
+          alignItems: "center",
+          justifyContent: "center",
+          height: 100,
+          width: "100%",
+        }}
+      >
+        <Text>No members of the team present with this role.</Text>
+      </Flex>
+    )}
+  </>
 );
 
 export default MembersTable;
