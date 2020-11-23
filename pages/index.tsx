21353cf1e41c65bb10d4ade72b1d88341fbd940e
mcsdevv <mail@mcs.dev>
2020-11-23 00:07:09 +0000

    add rubbish landing content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
+// * Modulz
+import { Heading } from "@modulz/radix";
+
 // * Components
 import MarketingLayout from "@components/layouts/pages/marketing";
 
 function Landing() {
-  return <h1>Draft & Publish</h1>;
+  return (
+    <>
+      <Heading size={2}>Draft your tweets.</Heading>
+      <Heading size={2}>Preview your OG images.</Heading>
+      <Heading size={2}>Have your team review.</Heading>
+      <Heading size={2}>Publish your tweets.</Heading>
+      <Heading size={2}>Access your metrics.</Heading>
+    </>
+  );
 }
 
 Landing.getLayout = (page: React.ReactNode) => (
