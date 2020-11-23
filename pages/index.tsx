081c42c25a6fa478fcad2d12162f30e0ef44fc57
mcsdevv <mail@mcs.dev>
2020-11-23 00:08:49 +0000

    column for landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 // * Modulz
-import { Heading } from "@modulz/radix";
+import { Flex, Heading } from "@modulz/radix";
 
 // * Components
 import MarketingLayout from "@components/layouts/pages/marketing";
 
 function Landing() {
   return (
-    <>
+    <Flex sx={{ flexDirection: "column" }}>
       <Heading size={2}>Draft your tweets.</Heading>
       <Heading size={2}>Preview your OG images.</Heading>
       <Heading size={2}>Have your team review.</Heading>
       <Heading size={2}>Publish your tweets.</Heading>
       <Heading size={2}>Access your metrics.</Heading>
-    </>
+    </Flex>
   );
 }
 
