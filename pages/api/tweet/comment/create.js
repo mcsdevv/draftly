63a4a7acc2d341730eba63697a1a663e57e91370
mcsdevv <mail@mcs.dev>
2020-11-27 23:40:29 +0000

    minor updates

--- pages/api/tweet/comment/create.js
+++ pages/api/tweet/comment/create.js
@@ -25,8 +25,6 @@ const createTweetComment = async (req, res, uid) => {
     },
   });
 
-  console.log(commentInserted);
-
   console.log("Added comment to:", twuid);
   res.status(200).json(commentInserted);
 };
