8f6e9a290f6e0e952580f41386671b8599650987
mcsdevv <mail@mcs.dev>
2021-03-07 22:40:42 +0000

    rename page

--- pages/_error.js
+++ pages/_error.js
@@ -1,7 +1,7 @@
 import NextErrorComponent from "next/error";
 import * as Sentry from "@sentry/node";
 
-const Error = ({ statusCode, hasGetInitialPropsRun, err }) => {
+const ErrorPage = ({ statusCode, hasGetInitialPropsRun, err }) => {
   if (!hasGetInitialPropsRun && err) {
     // getInitialProps is not called in case of
     // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
@@ -14,7 +14,7 @@ const Error = ({ statusCode, hasGetInitialPropsRun, err }) => {
   return <NextErrorComponent statusCode={statusCode} />;
 };
 
-Error.getInitialProps = async ({ res, err, asPath }) => {
+ErrorPage.getInitialProps = async ({ res, err, asPath }) => {
   const errorInitialProps = await NextErrorComponent.getInitialProps({
     res,
     err,
@@ -58,4 +58,4 @@ Error.getInitialProps = async ({ res, err, asPath }) => {
   return errorInitialProps;
 };
 
-export default Error;
+export default ErrorPage;
